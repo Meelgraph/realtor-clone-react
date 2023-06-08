c8261c31338cce446379b39ca65a707ad5297a5d
Your name <bogonkodick@gmail.com>
2023-06-08 08:57:53 +0300

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+// import SignOff from './pages SignOff';
+import Offers from './pages/Offers';
+import ForgotPassword from './pages/ForgotPassword';
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          {/* <Route path='/sign-off' element={<SignOff />} /> */}
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/offers' element={<Offers />} />
+        </Routes>
+      </Router>
     </>
   );
 }
