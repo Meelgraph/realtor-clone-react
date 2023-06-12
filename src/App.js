c064199229d17f0177c1d899b68cc857989bae2d
Your name <bogonkodick@gmail.com>
2023-06-12 12:27:10 +0300

    create header component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
-// import SignOff from './pages SignOff';
+import SignUp from './pages/SignUp';
 import Offers from './pages/Offers';
 import ForgotPassword from './pages/ForgotPassword';
+import Header from './pages/components/Header';
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/profile' element={<Profile />} />
           <Route path='/sign-in' element={<SignIn />} />
-          {/* <Route path='/sign-off' element={<SignOff />} /> */}
+          <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/offers' element={<Offers />} />
         </Routes>
